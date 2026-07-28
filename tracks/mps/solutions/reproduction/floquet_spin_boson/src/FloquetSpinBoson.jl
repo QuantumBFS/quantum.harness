431a099d26f3782af92c9058fe6a67fce6fd3171
Xiaoyang Hao <unionius5734@gmail.com>
2026-07-28 22:07:27 +0800

    feat: add matrix-free augmented Floquet operators

--- tracks/mps/solutions/reproduction/floquet_spin_boson/src/FloquetSpinBoson.jl
+++ tracks/mps/solutions/reproduction/floquet_spin_boson/src/FloquetSpinBoson.jl
@@ -5,6 +5,8 @@ include("model.jl")
 include("bath.jl")
 include("uniform_if.jl")
 include("checkpoint.jl")
+include("augmented_step.jl")
+include("floquet_operator.jl")
 include("reference_data.jl")
 include("redfield_magnus.jl")
 include("diagnostics.jl")
@@ -15,6 +17,10 @@ export RunConfig, period_grid, SpinBosonModel, SIGMA_X, SIGMA_Z,
        installed_uniformtempo_revision, uniform_if_metadata, uniform_if_key,
        uniform_if_build_settings, build_or_load_uniform_if,
        uniform_if_cache_path, load_or_build_uniform_if, atomic_save,
+       AugmentedLayout, composite_index, StepOperator, StepWorkspace,
+       apply_step!, apply_step_adjoint!, FloquetOperator, apply_period!,
+       apply_period_adjoint!, precompute_half_step_channels,
+       estimated_dense_bytes, dense_floquet,
        load_reference_curve, redfield_magnus!, redfield_magnus_paper_formula,
        run_fig2, parse_exact_baseline, render_refreshed_errors
 
--- tracks/mps/solutions/reproduction/floquet_spin_boson/test/runtests.jl
+++ tracks/mps/solutions/reproduction/floquet_spin_boson/test/runtests.jl
@@ -6,4 +6,5 @@ using .FloquetSpinBoson
 include("test_model.jl")
 include("test_uniform_if.jl")
 include("test_uniform_if_cli.jl")
+include("test_floquet_operator.jl")
 include("test_regression.jl")
